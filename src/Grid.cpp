b1d6e5130d83e076129bb790ecf7279cc42b9840
Salman Salim Abdul Salim Arakkaveetil <ptfs290h@f0378.nhr.fau.de>
2025-09-07 20:41:28 +0200

    Optimised dotProduct

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -339,7 +339,7 @@ double dotProduct(Grid *x, Grid *y, bool halo)
 #endif
 
     double dot_res = 0;
-    #pragma omp parallel for reduction(+:dot_res)
+    #pragma omp parallel for schedule(static) reduction(+:dot_res)
     for(int yIndex=shift; yIndex<x->numGrids_y(true)-shift; ++yIndex)
     {
         for(int xIndex=shift; xIndex<x->numGrids_x(true)-shift; ++xIndex)
