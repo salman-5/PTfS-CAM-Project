6fca4e57fe8bac610bd199b09827474dff18e51d
Salman Salim Abdul Salim Arakkaveetil <ptfs290h@fritz3.nhr.fau.de>
2025-09-07 15:21:13 +0200

    Job script for leaderboard

---
+++ job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --job-name=learderboard
+#SBATCH --time=01:00:00
+#SBATCH --constraint=hwperf
+#SBATCH --export=NONE
+unset SLURM_EXPORT_ENV
+
+# Load Intel compiler
+module load intel likwid
+make
+./leaderboard.sh "" "likwid-pin -c S0:0-35@S1:36-71"
